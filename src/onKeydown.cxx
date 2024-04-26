69eeb1ffd32492fc3c1d157ac087458c1a2301fb
Chase Taylor <creset200@gmail.com>
2024-04-26 00:43:43 -0600

    Add GPL notices to new files

--- src/onKeydown.cxx
+++ src/onKeydown.cxx
@@ -1,3 +1,23 @@
+/*
+  Copyright (C) 2024 Chase Taylor
+
+  This file is part of chTRACKER at path ./src/onKeydown.cxx
+
+  chTRACKER is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  chTRACKER is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  chTRACKER. If not, see <https://www.gnu.org/licenses/>.
+
+  Chase Taylor @ creset200@gmail.com
+*/
+
 #include "channel.hxx"
 #include "main.h"
 #include "order.hxx"
--- src/screenUpdate.cxx
+++ src/screenUpdate.cxx
@@ -1,3 +1,23 @@
+/*
+  Copyright (C) 2024 Chase Taylor
+
+  This file is part of chTRACKER at path ./src/screenUpdate.cxx
+
+  chTRACKER is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  chTRACKER is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  chTRACKER. If not, see <https://www.gnu.org/licenses/>.
+
+  Chase Taylor @ creset200@gmail.com
+*/
+
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_timer.h>
